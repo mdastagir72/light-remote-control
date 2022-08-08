21bb7346999296f052c14660a547e91b5667ea2a
Abdul Rahman Dastagir <102039148+mdastagir72@users.noreply.github.com>
2022-08-08 09:16:55 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(0)
 })
